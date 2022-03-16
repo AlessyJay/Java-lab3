c7ff4ac4f894dd019bac9c4fbef0335b43edfce7
Tony <alessyraffaello@gmail.com>
2022-03-16 17:48:46 +0700

    Task 4 1 to 100 fixed

--- EvenNumber.java
+++ EvenNumber.java
@@ -1,6 +1,6 @@
 class EvenNumber {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 1; i <= 100; i++) {
             if (i % 2 == 0) {
                 System.out.println(String.format("Even numbers from 0 to 100: %s", i));
             }
